f7faba34df5a8171dcabb45b450d594afc7fcb75
Matt yang <mattyang@Matts-MacBook-Pro.local>
2024-05-19 16:29:15 +1200

    Add support to multiple songs listened

--- spotify_recommend.py
+++ spotify_recommend.py
@@ -2,12 +2,17 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
+#Check if the names are in the library, artist attribute can take multiple singers'name
 def check_name_in_library(artists, library_name):
     result= []
     for item in library_name:
-        if artists in item:
-            result.append(0)
-        else:
+        found = False
+        for artist in artists:
+            if artist in item:
+                result.append(0)
+                found = True
+                break
+        if not found:
             result.append(1)
     return result
 
@@ -17,26 +22,44 @@ def get_diversity_scores(similarity_scores):
         result.append(0.5 * item)
     return result
 
+#Get similarity score between listened songs and song library and calculate the average similiarity score
+def get_similarity_scores(user_history, song_library):
+    result = []
+    similarity = cosine_similarity(user_history, song_library)
+    for song_index in range(len(song_library)):
+        total = 0
+        for index in range(len(user_history)):
+            total += similarity[index][song_index]
+        result.append(total / len(user_history))
+    return result
+
+def recommend_songs(user_history, singers, recommendNumber):
+    # Calculate similarity scores
+    similarity_scores = get_similarity_scores(user_history, song_library.iloc[0:, 8:18])
+
+    # Apply diversification penalty
+    diversification_penalty = get_diversity_scores(similarity_scores)
+
+    # Introduce bonus
+    bonus = 0.2 * np.array(check_name_in_library(singers, song_library['artists'].values))
+
+    # Final scores
+    final_scores = np.array(similarity_scores[0]) - np.array(diversification_penalty[0]) + bonus
+
+    song_library['score'] = final_scores.tolist()
+
+    # Sort and recommend
+    recommended_songs = song_library.sort_values(by='score', ascending=False)
+    if(recommendNumber > 0):
+        return recommended_songs.iloc[0:recommendNumber]
+    else:
+        return recommended_songs
+
 song_library = pd.read_csv("dataset.csv")
-user_history = song_library.iloc[58, 8 : 18].values
-singer = song_library.iloc[58]['artists']
+user_history = [song_library.iloc[58, 8 : 18].values, song_library.iloc[67, 8 : 18].values]
+singers = [song_library.iloc[58]['artists'],  song_library.iloc[67]['artists']]
 
-# Calculate similarity scores
-similarity_scores = cosine_similarity([user_history], song_library.iloc[0:, 8: 18])
-
-# Apply diversification penalty
-diversification_penalty = get_diversity_scores(similarity_scores)
-
-# Introduce bonus
-bonus = 0.2 * np.array(check_name_in_library(singer, song_library['artists'].values))
-
-# Final scores
-final_scores = np.array(similarity_scores[0]) - np.array(diversification_penalty[0]) + bonus
-
-song_library['score'] = final_scores.tolist()
-
-# Sort and recommend
-recommended_songs = song_library.sort_values(by='score', ascending=False)
+top_recommend_songs = recommend_songs(user_history, singers, 5)
 
 # Print recommended songs
-print(recommended_songs)
+print(top_recommend_songs)
